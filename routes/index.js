4896ce3ec5968438aae3a62f77632f7f13e72e11
tvanrys <tvanrys@fanshaweonline.ca>
2018-03-31 13:40:37 -0400

    simple redis example with session

---
+++ routes/index.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  if (req.session) {
+  	req.session.username = "me!";
+  }
+  res.render('index', { title: 'Express', name : req.session.username});
+});
+
+module.exports = router;
